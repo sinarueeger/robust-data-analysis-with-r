677283c8633792038aa56fd284d7ef05685ecbaa
Sina Rueeger <sina.rueeger@gmail.com>
2019-08-08 09:21:40 +0200

    adding package

--- workshop/5-package/5-package.R
+++ workshop/5-package/5-package.R
@@ -1,13 +1,65 @@
-## Aim: create map of EPFL that shows our location + the weather + pollution
-## https://freakonometrics.hypotheses.org/19213
-## https://github.com/sinarueeger/training-ground
-## https://dominicroye.github.io/en/2018/accessing-openstreetmap-data-with-r/
+## Aim: create a package in R
 
-## avalance data?
-## https://www.slf.ch/en/avalanches/destructive-avalanches-and-avalanche-accidents/accident-data.html
+library(usethis)
 
-## Task ------------------------------------------------
-## - Change pin location
+## 1. create the package skeleton
+create_package("~/tmp/epflcorpid")
+## https://www.epfl.ch/campus/services/communication/en/visual-identity/logotype/
 
-## Extra -----------------------------------------------
-## - make a leaflet with watercolors
+## 2. use git
+usethis::use_git()
+
+## 3. add a license
+usethis::use_mit_license()
+
+## 4. run check
+# install.packages("devtools")
+devtools::check()
+
+## 5. commit all files with git
+
+## 6. set up git + github
+usethis::use_github()
+## will update the DESCRIPTION file
+
+## 7. install the package
+devtools::install()
+
+## 8. add a rmarkdown readme file
+usethis::use_readme_rmd()
+## knit + commit + push
+
+## add R file
+usethis::use_r("epfl_colors.R")
+
+## usetestthat
+usethis::use_testthat
+
+## Rinse and repeat
+#load
+# document
+#devtools::install()
+#check
+
+## adapt Readme file
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+## 9. clean up if this was only a demo
+## install.packages("fs")
+## fs::dir_delete("~/tmp/mypackage")
